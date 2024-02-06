eb513d35fc91dd4edbfebbc1fbea42a6ba849a77
amishad7 <amiahd488@email.com>
2024-02-06 15:57:30 +0530

    project created

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/':=>SplashScreen(),
+      }
+    ),
+  );
+}
